faf46b5cf865db00ddfaaa833d26facd4a7d28ef
halle <jinhao11@163.com>
2021-02-23 14:52:31 +0800

    add lombok pom,msg body

--- raft-core-v0.0.1/src/main/java/org/halle/rpc/ElectionMsg.java
+++ raft-core-v0.0.1/src/main/java/org/halle/rpc/ElectionMsg.java
@@ -1,13 +1,10 @@
 package org.halle.rpc;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class RequestBody<T> {
-    private T data;
-    private int requestType;
-
+public class ElectionMsg {
+    private int currentTerm;
 }
